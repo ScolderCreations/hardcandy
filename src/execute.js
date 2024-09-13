7e3323ac9c7ca1474d78a0cf276bf6a0c7e76365
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2024-09-13 10:23:12 -0700

    pretty

--- src/execute.js
+++ src/execute.js
@@ -1,12 +1,14 @@
-const tk = require( "./tokenizer.js")
+const tk = require("./tokenizer.js")
 const fs = require("node:fs")
 fs.readFile("../test/test.hc", (e, data) => {
     if (e) throw e;
     file = String(data);
 })
+
 let tokens = tk.tokenize(file)
 const variables = {}
 var tokenindex = 0
+
 function interpretCommand(command, argu) {
     switch (command) {
         case "send":
